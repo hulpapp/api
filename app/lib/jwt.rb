76abe97f2e6f0596edeb13bad3d29b06d0084488
Daniel Costa <dhrc1997@gmail.com>
2021-08-17 09:29:56 -0300

    jwt prod and dev

--- app/lib/jwt.rb
+++ app/lib/jwt.rb
@@ -2,20 +2,25 @@ require 'net/http'
 require 'uri'
 
 class Jwt
+
+  @@domain = (Rails.env.test? or Rails.env.development?) ? Rails.application.credentials.auth0_dev[:domain] : Rails.application.credentials.auth0_prod[:domain]
+  @@api_identifier =(Rails.env.test? or Rails.env.development?) ? Rails.application.credentials.auth0_dev[:api_identifier] : Rails.application.credentials.auth0_prod[:api_identifier]
+
   def self.verify(token)
+
     JWT.decode(token, nil,
                true, # Verify the signature of this token
                algorithm: 'RS256',
-               iss: Rails.application.credentials.auth0[:domain],
+               iss: @@domain,
                verify_iss: true,
-               aud: Rails.application.credentials.auth0[:api_identifier],
+               aud: @@api_identifier,
                verify_aud: true) do |header|
       jwks_hash[header['kid']]
     end
   end
 
   def self.jwks_hash
-    jwks_raw = Net::HTTP.get URI("#{Rails.application.credentials.auth0[:domain]}.well-known/jwks.json")
+    jwks_raw = Net::HTTP.get URI("#{@@domain}.well-known/jwks.json")
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
     Hash[
       jwks_keys
