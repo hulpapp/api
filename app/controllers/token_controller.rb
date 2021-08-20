035f560a3defc34f60745112039ec3b2b0a06794
Daniel Costa <dhrc1997@gmail.com>
2021-08-20 09:03:09 -0300

    trying to make a get token

---
+++ app/controllers/token_controller.rb
@@ -0,0 +1,35 @@
+require 'openssl'
+require 'net/http'
+require 'uri'
+
+class TokenController < ApplicationController
+
+  @@domain = (Rails.env.test? or Rails.env.development?) ? Rails.application.credentials.auth0_dev[:domain] : Rails.application.credentials.auth0_prod[:domain]
+  @@api_identifier =(Rails.env.test? or Rails.env.development?) ? Rails.application.credentials.auth0_dev[:api_identifier] : Rails.application.credentials.auth0_prod[:api_identifier]
+  @@client_id = (Rails.env.test? or Rails.env.development?) ? Rails.application.credentials.auth0_dev[:client_id] : Rails.application.credentials.auth0_prod[:client_id]
+  @@client_secret = (Rails.env.test? or Rails.env.development?) ? Rails.application.credentials.auth0_dev[:client_secret] : Rails.application.credentials.auth0_prod[:client_secret]
+
+
+  def authorize
+
+    url = URI("#{@@domain}authorize?response_type=code&client_id=#{@@client_id}&audience=#{@@api_identifier}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = false
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+
+    request = Net::HTTP::Post.new(url)
+    request["content-type"] = 'application/json'
+
+
+    response = http.request(request)
+
+    render json: response.read_body
+
+
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/isalive', to: 'is_alive#index'
+  get '/token', to: 'token#authorize'
 end
