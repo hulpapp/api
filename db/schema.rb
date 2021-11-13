932d28a0b7a52bb5bdf6d1c21193400477aaadbe
Daniel Costa <dhrc1997@gmail.com>
2021-11-13 14:00:56 -0300

    neighborhood listed at addresses controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_132403) do
+ActiveRecord::Schema.define(version: 2021_11_13_165954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_11_13_132403) do
     t.string "country"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "neighborhood"
   end
 
   create_table "beneficiaries", force: :cascade do |t|
