e09dda820c27ee50eb9e6fd07ae70716c01a731f
Mr. Daniel Costa <dhrc1997@gmail.com>
2022-10-15 16:44:16 -0300

    heroku deployment - try

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins "github.io", 'localhost:3000', '127.0.0.1:3000'
 
-    resource '*',
+    resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
