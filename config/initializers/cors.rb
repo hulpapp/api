32091fa20664879f46569344af9317da9c9b3275
Mr. Daniel Costa <dhrc1997@gmail.com>
2022-10-16 12:41:47 -0300

    credentials updt - cors - minorfix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
 
-    origins Rails.application.credentials.hosts.remote
+    origins "hulpapp.github.io"
 
     resource "*",
       headers: :any,
